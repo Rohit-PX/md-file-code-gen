0285b50592c08d90179e12c9a15e48ef45c6c01d
Rohit-PX <rohit.kulk@gmail.com>
2024-05-08 19:56:33 -0700

    add SSH ability
    Signed-off-by: Rohit-PX <rohit.kulk@gmail.com>

--- main.go
+++ main.go
@@ -1,25 +1,17 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
-	"os/exec"
 
-	"github.com/Rohit-PX/md-file-code-gen/markdownutils"
+	"github.com/md-file-code-gen/markdownutils"
 
 	"github.com/golang-commonmark/markdown"
 )
 
-var ArtifactDirectory string = "artifacts"
-var YamlFileName string = ArtifactDirectory + "/docTest.yaml"
-var KubectlCmdFileName string = ArtifactDirectory + "/kubectlCmd.sh"
-var kubectlCmd string = "kubectl"
-var pxctlCmd string = "pxctl"
 var cmdType, ipAddr string
 
 func main() {
@@ -42,7 +34,8 @@ func main() {
 	md := markdown.New(markdown.XHTMLOutput(true), markdown.Nofollow(true))
 	tokens := md.Parse(body)
 
-	yamlFile, DocCmdFile, err := markdownutils.CreateArtifactFiles(ArtifactDirectory, YamlFileName, KubectlCmdFileName)
+	yamlFile, DocCmdFile, err := markdownutils.CreateArtifactFiles(markdownutils.ArtifactDirectory,
+		markdownutils.YamlFileName, markdownutils.CmdFileName)
 	if err != nil {
 		log.Fatalf("unable to create yaml/cmd files: %v", err)
 	}
@@ -51,7 +44,7 @@ func main() {
 		yamlFile.Close()
 		DocCmdFile.Close()
 	}()
-	//Print the result
+
 	for _, t := range tokens {
 		snippet := markdownutils.GetSnippet(t)
 
@@ -71,61 +64,14 @@ func main() {
 	// Read kubeconfig and set KUBECONFIG environment variable accordingly
 	os.Setenv("KUBECONFIG", kubeconfigFilePath)
 
-	// Apply yaml file and execute kubectl command script
-
-	var cmd *exec.Cmd
-	var out, kubeErr bytes.Buffer
-	if cmdType == kubectlCmd {
-		cmd = exec.Command("kubectl", "apply", "-f", YamlFileName)
-		cmd.Stdout = &out
-		cmd.Stderr = &kubeErr
-	} else if cmdType == pxctlCmd {
-		// open the file containing just pxctl commands
-		file, err := os.Open(KubectlCmdFileName)
-		if err != nil {
-			log.Fatalf("Error opening file:", err)
-		}
-		// Create a new Scanner to read the file.
-		scanner := bufio.NewScanner(file)
-		// Set the split function to the default ScanLines
-		scanner.Split(bufio.ScanLines)
-
-		// Loop over all lines in the file.
-		for scanner.Scan() {
-			line := scanner.Text()
-			fmt.Println(line)
-			pxSSH := fmt.Sprintf("ssh root@%s '%s'\n", ipAddr, line) // Print each line, or process it as needed.
-			cmd = exec.Command(pxSSH)
-			cmd.Stdout = &out
-			cmd.Stderr = &kubeErr
-
-			if err := cmd.Run(); err != nil {
-				fmt.Println(out.String())
-				log.Fatal(cmd.Stderr)
-			}
-		}
-
-		//cmd = exec.Command("bash", KubectlCmdFileName)
-		//cmd.Stdout = &out
-		//cmd.Stderr = &kubeErr
-
-		//if err := cmd.Run(); err != nil {
-		//	fmt.Println(out.String())
-		//	log.Fatal(cmd.Stderr)
-		//}
+	info := markdownutils.ExecutableInfo{
+		CommandType: cmdType,
+		IpAddr:      ipAddr,
 	}
 
-	//if err := cmd.Run(); err != nil {
-	//	log.Fatal(cmd.Stderr)
-	//}
-	//fmt.Println(out.String())
-
-	//cmd = exec.Command("bash", KubectlCmdFileName)
-	//cmd.Stdout = &out
-	//cmd.Stderr = &kubeErr
-
-	//if err := cmd.Run(); err != nil {
-	//	log.Fatal(cmd.Stderr)
-	//}
-	//fmt.Println(out.String())
+	// Apply yaml file and execute kubectl command script
+	err = markdownutils.ExecuteCmdFile(&info)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- markdownutils/markdown.go
+++ markdownutils/markdown.go
@@ -1,17 +1,42 @@
 package markdownutils
 
 import (
-	"io/ioutil"
-	"net/http"
+	"bufio"
+	"bytes"
+	"fmt"
+	"log"
 	"os"
+	"os/exec"
 
 	"github.com/golang-commonmark/markdown"
+	"golang.org/x/crypto/ssh"
+)
+
+const (
+	kubectlCmd        = "kubectl"
+	pxctlCmd          = "pxctl"
+	ArtifactDirectory = "artifacts"
+	YamlFileName      = ArtifactDirectory + "/docTest.yaml"
+	CmdFileName       = ArtifactDirectory + "/commands.sh"
+	NodeUser          = "root"
+	NodePassword      = "Password1"
 )
 
 // Snippet represents the snippet we will output.
 type Snippet struct {
 	Content string
 	Lang    string
+}
+
+// ExecutableInfo contains information about commands and where to execute them
+type ExecutableInfo struct {
+	CommandType string `json:"commandtype"`
+	IpAddr      string `json:"ipaddr"`
+}
+
+type Conn struct {
+	Session ssh.Session
+	ExecutableInfo
 }
 
 // getSnippet extract only code Snippet from markdown object.
@@ -36,17 +61,6 @@ func GetSnippet(tok markdown.Token) Snippet {
 	return Snippet{}
 }
 
-// readFromWeb call the given url and return the content of the readme.
-func readFromWeb(url string) ([]byte, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	return ioutil.ReadAll(resp.Body)
-}
-
 // CreateArtifactFiles creates directories, yaml and sh files required for testing the doc
 func CreateArtifactFiles(dirName, yamlFileName, cmdFileName string) (*os.File, *os.File, error) {
 	err := os.MkdirAll("artifacts", os.ModePerm)
@@ -54,15 +68,166 @@ func CreateArtifactFiles(dirName, yamlFileName, cmdFileName string) (*os.File, *
 		return nil, nil, err
 	}
 
-	yamlFile, err := os.Create("artifacts/docTest.yaml")
+	yamlFile, err := os.Create(yamlFileName)
 	if err != nil {
 		return nil, nil, err
 	}
 
-	cmdFile, err := os.Create("artifacts/kubectlCmd.sh")
+	cmdFile, err := os.Create(cmdFileName)
 	if err != nil {
 		return nil, nil, err
 	}
 
 	return yamlFile, cmdFile, err
 }
+
+// ExecuteCmdFile
+//
+//		for kubectl: 1. applies yaml file and
+//	              2. executes kubectl commands from command file using provided kubeconfig file
+//		for pxctl: executes pxctl commands by creating ssh connection to the provided ip addres
+func ExecuteCmdFile(execInfo *ExecutableInfo) error {
+	var cmd *exec.Cmd
+	var out, kubeErr bytes.Buffer
+	myConn := NewConnection(execInfo.IpAddr)
+	switch execInfo.CommandType {
+	case kubectlCmd:
+		log.Printf("Kubectl command type selected.")
+		cmd = exec.Command("kubectl", "apply", "-f", YamlFileName)
+		cmd.Stdout = &out
+		cmd.Stderr = &kubeErr
+		if err := cmd.Run(); err != nil {
+			return fmt.Errorf("failed to execute kubectl command error: %v", cmd.Stderr)
+		}
+		fmt.Println(out.String())
+
+		err := ReadFileRunCmd(myConn, CmdFileName, false)
+		if err != nil {
+			return err
+		}
+
+	case pxctlCmd:
+		log.Printf("pxctl command type selected. Reading file: %s", CmdFileName)
+		file, err := os.Open(CmdFileName)
+		if err != nil {
+			log.Fatalf("Error opening file: %v", err)
+		}
+		defer file.Close()
+		scanner := bufio.NewScanner(file)
+
+		for scanner.Scan() {
+			line := scanner.Text()
+			if line != "" {
+				log.Printf("pxctl command: %s", line)
+				out, err := myConn.RunSSHCmd(line)
+				if err != nil {
+					return fmt.Errorf("failed to run command: \"%s\" on: %s, err: %v", line, execInfo.IpAddr, err)
+				}
+				log.Printf("PXCTL output: %s", out)
+			}
+		}
+	}
+	return nil
+}
+
+func ReadFile(fileName string) {
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		log.Panicf("failed reading data from file: %s", err)
+	}
+	fmt.Println(data)
+}
+
+func NewConnection(ip string) Conn {
+	sess, err := GetSSHSession(ip)
+	if err != nil {
+		log.Fatalf("failed to get new session for %s: err: %v", ip, err)
+	}
+	return Conn{Session: sess}
+}
+
+func GetSSHSession(ip string) (ssh.Session, error) {
+	config := &ssh.ClientConfig{
+		User: NodeUser,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(NodePassword),
+		},
+
+		// It is very important to verify the server's host key in production
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	// Connect to the SSH server
+	connection, err := ssh.Dial("tcp", ip+":22", config)
+	if err != nil {
+		return ssh.Session{}, fmt.Errorf("failed to dial: %s. err: %v", ip, err)
+	}
+
+	// Open a session
+	newSession, err := connection.NewSession()
+	if err != nil {
+		return ssh.Session{}, fmt.Errorf("failed to create session: %s. err: %v", ip, err)
+	}
+
+	return *newSession, nil
+}
+
+func (c *Conn) RunSSHCmd(cmd string) (string, error) {
+	output, err := c.Session.CombinedOutput(cmd)
+	return string(output), err
+}
+
+func RunCmd(cmd string) (string, error) {
+	var execCmd *exec.Cmd
+	var out, kubeErr bytes.Buffer
+
+	execCmd = exec.Command(cmd)
+	execCmd.Stdout = &out
+	execCmd.Stderr = &kubeErr
+
+	if err := execCmd.Run(); err != nil {
+		return out.String(), fmt.Errorf("failed to execute cmd: %s, err: %v", cmd, err)
+	}
+	return out.String(), nil
+}
+
+func ReadFileRunCmd(myConn Conn, CmdFileName string, isSSH bool) error {
+	file, err := os.Open(CmdFileName)
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line != "" {
+			if isSSH {
+				out, err := myConn.RunSSHCmd(line)
+				if err != nil {
+					return fmt.Errorf("failed to run command: \"%s\" on: %s, err: %v", line, myConn.IpAddr, err)
+				}
+				log.Printf("PXCTL output: %s", out)
+			} else {
+				out, err := RunCmd(line)
+				if err != nil {
+					return fmt.Errorf("failed to run command: \"%s\" on: %s, err: %v", line, myConn.IpAddr, err)
+				}
+				log.Printf("PXCTL output: %s", out)
+
+			}
+		}
+	}
+	return nil
+}
+
+//// readFromWeb call the given url and return the content of the readme.
+//func readFromWeb(url string) ([]byte, error) {
+//	resp, err := http.Get(url)
+//	if err != nil {
+//		return nil, err
+//	}
+//	defer resp.Body.Close()
+//
+//	return ioutil.ReadAll(resp.Body)
+//}
